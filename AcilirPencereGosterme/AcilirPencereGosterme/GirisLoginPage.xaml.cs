583f8315a1625ca67b5cce3cbeda28a1db43c16f
celalaksu <32665644+celalaksu@users.noreply.github.com>
2020-05-03 23:48:42 +0300

    Proje dosyası ekle.

---
+++ AcilirPencereGosterme/AcilirPencereGosterme/GirisLoginPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using Rg.Plugins.Popup.Animations;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AcilirPencereGosterme
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GirisLoginPage : PopupPage
+    {
+        public GirisLoginPage()
+        {
+            InitializeComponent();
+            //Animation = new ScaleAnimation();
+            
+        }
+
+        private async void GirisButton_Clicked(object sender, EventArgs e)
+        { 
+            await Navigation.PushModalAsync(new GirisBasarili());
+            await PopupNavigation.Instance.PopAllAsync();           
+        }
+
+        private void vazgecButton_Clicked(object sender, EventArgs e)
+        {
+            AcilirPencereKapat();
+        }
+
+        private async void AcilirPencereKapat()
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+    
+        protected override void OnAppearingAnimationBegin()
+        {
+            base.OnAppearingAnimationBegin();
+
+            //FrameContainer.HeightRequest = -1;
+
+            if (!IsAnimationEnabled)
+            {
+             /*   
+                CloseImage.Rotation = 0;
+                CloseImage.Scale = 1;
+                CloseImage.Opacity = 1;
+                
+                */
+                //girisButton.Scale = 1;
+                girisButton.Opacity = 1;
+                vazgecButton.Opacity = 1;
+
+                kullaniciAdiEntry.TranslationX = sifreEntry.TranslationX = 0;
+                kullaniciAdiEntry.Opacity = sifreEntry.Opacity = 1;
+
+                return;
+            }
+
+            /*
+            CloseImage.Rotation = 30;
+            CloseImage.Scale = 0.3;
+            CloseImage.Opacity = 0;
+            */
+
+            //girisButton.Scale = 0.3;
+            girisButton.Opacity = 0;
+            vazgecButton.Opacity = 0;
+
+            kullaniciAdiEntry.TranslationX = sifreEntry.TranslationX = -10;
+            kullaniciAdiEntry.Opacity = sifreEntry.Opacity = 0;
+        }
+
+        
+
+        protected override async Task OnAppearingAnimationEndAsync()
+        {
+            
+            if (!IsAnimationEnabled)
+                return;
+
+            var translateLength = 400u;
+
+            await Task.WhenAll(
+                kullaniciAdiEntry.TranslateTo(0, 0, easing: Easing.SpringOut, length: translateLength),
+                kullaniciAdiEntry.FadeTo(1),
+                (new Func<Task>(async () =>
+                {
+                    await Task.Delay(200);
+                    await Task.WhenAll(
+                        sifreEntry.TranslateTo(0, 0, easing: Easing.SpringOut, length: translateLength),
+                        sifreEntry.FadeTo(1));
+
+                }))());
+
+            await Task.WhenAll(
+                /*
+                 CloseImage.FadeTo(1),
+                 CloseImage.ScaleTo(1, easing: Easing.SpringOut),
+                 CloseImage.RotateTo(0),
+                */ 
+                girisButton.ScaleTo(1),
+                girisButton.FadeTo(1),
+                vazgecButton.ScaleTo(1),
+                vazgecButton.FadeTo(1));
+        }
+
+        
+    }
+}
---
+++ AcilirPencereGosterme/AcilirPencereGosterme/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AcilirPencereGosterme
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void AcilirPencere_Clicked(object sender, EventArgs e)
+        {
+            bool cevap = await DisplayAlert("Soru", "Soru sorma penceresi çalıştı mı?", "Evet", "Hayır");
+            if (cevap)
+            {
+                await DisplayAlert("Cevabınız olumlu", "Evet diyerek cevapladınız.", "Tamam");
+            }
+            else
+            {
+                await DisplayAlert("Cevabınız olumsuz", "Hayır diyerek cevapladınız.", "Tamam");
+            }
+        }
+
+        private async void SeceneklerButton_Clicked(object sender, EventArgs e)
+        {
+            string secilenIslem = await DisplayActionSheet("Sosyal medyayı seçiniz", "Vazgeç", null, "Facebook", "Twitter", "Google");
+
+            await DisplayAlert("Seçiminiz", $"{secilenIslem} 'i seçtiniz", "Tamam");
+        }
+
+        private async void VeriAlButton_Clicked(object sender, EventArgs e)
+        {
+            string cevap = await DisplayPromptAsync("Xamarin", "Xamarin'le işler nasıl gidiyor?");
+            await DisplayAlert("Yorumunuz", $"Cevabınız \"{cevap}\"", "Tamam");
+        }
+
+        private async void Giris_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new GirisLoginPage());
+        }
+
+        private async void OzelAnimasyonButton_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PushAsync(new OzelAnimasyonPage());
+        }
+    }
+}
